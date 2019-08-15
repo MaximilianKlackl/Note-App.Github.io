7802958b096859d82791458d04916e240572c0aa
MaximilianKlackl <46849661+MaximilianKlackl@users.noreply.github.com>
2019-08-15 18:52:34 +0200

    h

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ var btn = document.getElementById("btn");
 var textarea = document.getElementById("textarea");
 var select = document.getElementById("color");
 var btnBackground = document.getElementById("btn-background");
+var btnHistory = document.getElementById("clearHistory");
 
+//load
 loadNotes();
 loadStyling();
 loadBackground();
+loadHistory();
  
+
+//button listener
 btn.addEventListener("click", createNote);
 btnBackground.addEventListener("click", changeBackground);
+btnHistory.addEventListener("click", clearHistory);
 
 //change styling on option
 changeColor(localStorage.getItem("color"));
@@ -60,7 +66,8 @@ function loadStyling()
 function loadNotes()
 { 
   Object.keys(localStorage).forEach(function(key){
-    if(key != "color" && key != "background")
+
+    if(key != "color" && key != "background" && key.substring(0, 8) != "history-")
     {
       let containerDiv = document.getElementById("notes-container"); 
 
@@ -144,6 +151,8 @@ function deleteNote(e)
       localStorage.removeItem(key);
     }
   });
+
+  addHistory(text);
 }
 
 function changeStyling()
@@ -189,7 +198,6 @@ function changeColor(color)
   let notes = document.querySelectorAll(".note");
 
   notes.forEach(e => {
-    console.log(color);
       e.style.backgroundColor = color;
   } )
 
@@ -197,6 +205,7 @@ function changeColor(color)
   btn.style.backgroundColor = color;
   select.style.backgroundColor = color;
   btnBackground.style.backgroundColor = color;
+  btnHistory.style.backgroundColor = color;
 }
 
 function displayRightSelect(select)
@@ -222,6 +231,108 @@ function displayRightSelect(select)
   }
 }
 
+function DrowpDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
+function addHistory(text)
+{
+  localStorage.setItem("history-" + text, text);
+  addDrowdown(text)
+}
 
+function addDrowdown(text)
+{
+  let dropDown = document.getElementById("myDropdown"); 
+
+  //Create Elements
+  let newDiv = document.createElement("div");
+  let node = document.createTextNode(text);
+
+  //Appending Elements
+  newDiv.append(node);
+  dropDown.append(newDiv);
+
+  //event listener for delete button
+  addListenerPullBack(newDiv);
+}
+
+function addListenerPullBack(div) 
+{
+  div.addEventListener('click', function (e) 
+  {
+    let containerDiv = document.getElementById("notes-container"); 
+    let text = e.target.innerText;
+
+    //Create Elements
+    let newDiv = document.createElement("div");
+    let para = document.createElement("p");
+    let newNote = document.createTextNode(text); 
+    let deleteButton = document.createElement("div");
+
+    //Styling Elements
+    newDiv.classList.add("note");
+    deleteButton.classList.add("deleteButton");
+
+    //Appending Elements
+    para.appendChild(newNote);
+    newDiv.appendChild(deleteButton);
+    newDiv.appendChild(para);
+    containerDiv.append(newDiv);
+
+    localStorage.setItem(text, text);
+    localStorage.removeItem("history-" + text, text);
+
+    //event listener for delete button
+    addListenerDeleteButton(deleteButton,text);
+
+    changeColor(localStorage.getItem("color"));
+
+    let parent = e.target.parentNode;
+    parent.removeChild(e.target);
+  });
+}
+
+function loadHistory()
+{
+  Object.keys(localStorage).forEach(function(key){
+
+    if(key.substring(0,8) == "history-")
+    {
+      addDrowdown(key.substring(8,key.length));
+    }
+  });
+}
+
+function clearHistory()
+{
+  var dropDown = document.getElementById("myDropdown"); 
+
+  while(dropDown.childElementCount > 1)
+  {
+    dropDown.removeChild(dropDown.lastChild);
+  }
+
+  Object.keys(localStorage).forEach(function(key){
+
+    if(key.substring(0,8) == "history-")
+    {
+      localStorage.removeItem(key);
+
+    }
+  });
+}
